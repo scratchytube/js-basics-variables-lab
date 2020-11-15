23e85886396d0bcbe235b68b57d0abb07000c2cf
Scratchytube <bpsahota@gmail.com>
2020-11-15 12:19:34 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+// let companyName = 'Scuber';
+// let companyName = 'Scuber';
+
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith'
+
